82e12e4603cd6c7fb5a0c843d7cd9d72a436e5ba
Ian Murray <ian.c.murray.@gmail.com>
2012-08-19 21:56:40 +0100

    A Revisioned domain model trait

---
+++ src/main/scala/models/Revisioned.scala
@@ -0,0 +1,14 @@
+package freeclimb.models
+
+/**
+ * A Revisioned[T] is a particular revision of a domain model, T.
+ *
+ * @param revision is a strictly-increasing integer.  Gaps in the sequence are
+ *        fine.
+ *
+ * @param model is the underlying model value at the given revision.
+ */
+trait Revisioned[T] {
+  val revision: Long
+  val model: T
+}
