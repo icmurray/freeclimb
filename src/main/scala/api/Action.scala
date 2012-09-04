b00764369e21b6bf9112f9105cccbbc8bc47f952
Ian Murray <ian.c.murray.@gmail.com>
2012-09-04 22:20:01 +0100

    Renamed Action to ActionT
    And define a type alias for Action that is ActionT with the Id monad.

--- src/main/scala/api/Action.scala
+++ src/main/scala/api/Action.scala
@@ -6,7 +6,7 @@ import Scalaz._
 import freeclimb.models._
 import freeclimb.sql.IsolationLevel
 
-case class Action[M[+_], +A, -I <: IsolationLevel](g: DbSession[I] => M[A]) {
+case class ActionT[M[+_], +A, -I <: IsolationLevel](g: DbSession[I] => M[A]) {
 
   def apply(s: DbSession[I]) = g(s)
 
@@ -30,11 +30,11 @@ case class Action[M[+_], +A, -I <: IsolationLevel](g: DbSession[I] => M[A]) {
     }
   }
 
-  def map[B](f: A => B)(implicit F: Functor[M]): Action[M,B,I] = Action[M,B,I]{ s =>
+  def map[B](f: A => B)(implicit F: Functor[M]): ActionT[M,B,I] = ActionT[M,B,I]{ s =>
     F.map(g(s))(f)
   }
 
-  def flatMap[B, II <: I](f: A => Action[M,B,II])(implicit M: Bind[M]): Action[M,B,II] = Action[M,B,II]{ s =>
+  def flatMap[B, II <: I](f: A => ActionT[M,B,II])(implicit M: Bind[M]): ActionT[M,B,II] = ActionT[M,B,II]{ s =>
     M.bind(g(s))(f(_)(s))
   }
 
@@ -44,19 +44,19 @@ case class Action[M[+_], +A, -I <: IsolationLevel](g: DbSession[I] => M[A]) {
  * This trait defines some implicit conversions for inter-operating with
  * scalaz's type classes.
  */
-trait ActionInstances {
+trait ActionTInstances {
 
   /**
-   * Ensure that Action is seen as a Monad and a Functor when using scalaz.
+   * Ensure that ActionT is seen as a Monad and a Functor when using scalaz.
    * For example, this is necessary when using it with monad transformers.
    */
-  implicit def actionInstance[M[+_], I <: IsolationLevel](implicit M: Monad[M]) = new Monad[({type l[a] = Action[M, a, I]})#l] with Functor[({type l[a] = Action[M, a, I]})#l] {
-    override def bind[A, B](fa: Action[M,A,I])(f: A => Action[M,B,I]): Action[M,B,I] = fa flatMap f
-    override def point[A](a: => A): Action[M,A,I] = Action[M,A,I] { _ => M.point(a) }
+  implicit def actionInstance[M[+_], I <: IsolationLevel](implicit M: Monad[M]) = new Monad[({type l[a] = ActionT[M, a, I]})#l] with Functor[({type l[a] = ActionT[M, a, I]})#l] {
+    override def bind[A, B](fa: ActionT[M,A,I])(f: A => ActionT[M,B,I]): ActionT[M,B,I] = fa flatMap f
+    override def point[A](a: => A): ActionT[M,A,I] = ActionT[M,A,I] { _ => M.point(a) }
   }
 }
 
 /**
  * Companion object that captures all the implicits defined above
  */
-object Action extends ActionInstances
+object ActionT extends ActionTInstances
--- src/main/scala/api/package.scala
+++ src/main/scala/api/package.scala
@@ -5,12 +5,14 @@ import Scalaz._
 
 import freeclimb.common._
 import freeclimb.models._
+import freeclimb.sql.IsolationLevel
 
 package object api {
   
   /**
    * Some type synonyms to help tidy the function signatures.
    */
+  type Action[A,I <: IsolationLevel] = ActionT[Id,A,I]
   type ActionResult[T] = DisjunctionT[ApiAction, ConcurrentAccess, Revisioned[T]]
   type ActionSuccess[T] = DisjunctionT[ApiAction, ConcurrentAccess, Unit]
 
