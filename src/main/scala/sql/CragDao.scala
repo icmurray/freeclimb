cc359f8225046040eed50d80622a8a298ef5471e
Ian Murray <ian.c.murray.@gmail.com>
2012-09-05 21:24:52 +0100

    Flesh out the exception handling
    Needs work to test that it works, and to clean it up.

--- src/main/scala/sql/CragDao.scala
+++ src/main/scala/sql/CragDao.scala
@@ -1,6 +1,6 @@
 package freeclimb.sql
 
-import java.sql.Connection
+import java.sql.{SQLException, Connection}
 
 import anorm._
 
@@ -10,6 +10,8 @@ import Scalaz._
 import freeclimb.api._
 import freeclimb.common._
 import freeclimb.models._
+
+import freeclimb.sql.SqlError._
 
 /**
  * The Data Access Object for Crags.
@@ -22,19 +24,30 @@ trait CragDao extends Repository[Crag] {
   override def create(crag: Crag) = ActionT { session =>
     implicit val connection = session.dbConnection
 
-    val revision: Option[Long] = SQL(
-      """
-      INSERT INTO crags(name, title)
-        VALUES ({name}, {title})
-      """
-    ).on("name"  -> crag.name,
-         "title" -> crag.title
-    ).executeInsert()
+    try{
 
-    revision match {
-      case Some(rev) => new Revisioned[Crag](rev, crag).right
-      case None      => ConcurrentUpdate().left
+      val revision: Option[Long] = SQL(
+        """
+        INSERT INTO crags(name, title)
+          VALUES ({name}, {title});
+        """
+      ).on("name"  -> crag.name,
+           "title" -> crag.title
+      ).executeInsert()
+
+      revision match {
+        case Some(rev) => new Revisioned[Crag](rev, crag).right
+        case None      => ConcurrentUpdate().left
+      }
+
+    } catch {
+      case e: SQLException => e.sqlError match {
+        case Some(UniqueViolation) => connection.rollback() ; ConcurrentUpdate().left
+        case _                     => connection.rollback() ; throw e
+      }
+      case e => connection.rollback() ; throw e
     }
+
   }
 
   def get(name: String): Action[Option[Revisioned[Crag]], TransactionReadCommitted] = Action { session =>
---
+++ src/main/scala/sql/SqlError.scala
@@ -0,0 +1,20 @@
+package freeclimb.sql
+
+import java.sql.SQLException
+
+trait WithSqlError {
+  val sqlError: Option[SqlError.SqlError]
+}
+
+object SqlError extends Enumeration {
+  type SqlError = Value
+  val UniqueViolation = Value("23505")
+  val CheckViolation = Value("23514")
+  val SerializationFailure = Value("40001")
+
+  implicit def sqlException2SqlError(e: SQLException): WithSqlError = new WithSqlError {
+    val sqlError = SqlError.values.find(_.toString == e.getSQLState)
+  }
+}
+
+
