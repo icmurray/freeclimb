3e5bbe99227518d8e63547303fc938b9031663ff
Ian Murray <ian.c.murray@gmail.com>
2012-09-10 18:18:16 +0100

    The tests helped flush out some problems.

--- src/main/scala/sql/CragDao.scala
+++ src/main/scala/sql/CragDao.scala
@@ -3,6 +3,7 @@ package freeclimb.sql
 import java.sql.{SQLException, Connection}
 
 import anorm._
+import anorm.SqlParser._
 
 import scalaz._
 import Scalaz._
@@ -13,6 +14,8 @@ import freeclimb.models._
 
 import freeclimb.sql.SqlError._
 
+object CragDao extends CragDao
+
 /**
  * The Data Access Object for Crags.
  *
@@ -20,25 +23,24 @@ import freeclimb.sql.SqlError._
  */
 trait CragDao extends Repository[Crag] {
 
-  /** TODO: SQL exception handling. */
   override def create(crag: Crag) = ActionT { session =>
     implicit val connection = session.dbConnection
 
     try{
 
-      val revision: Option[Long] = SQL(
+      val nextRevision: Long = SQL("SELECT nextval('crag_revision_seq');").as(scalar[Long].single)
+
+      SQL(
         """
-        INSERT INTO crags(name, title)
-          VALUES ({name}, {title});
+        INSERT INTO crags(name, title, revision)
+          VALUES ({name}, {title}, {revision});
         """
-      ).on("name"  -> crag.name,
-           "title" -> crag.title
+      ).on("name"     -> crag.name,
+           "title"    -> crag.title,
+           "revision" -> nextRevision
       ).executeInsert()
 
-      revision match {
-        case Some(rev) => new Revisioned[Crag](rev, crag).right
-        case None      => ConcurrentUpdate().left
-      }
+      Revisioned[Crag](nextRevision, crag).right
 
     } catch {
       case e: SQLException => e.sqlError match {
@@ -50,10 +52,72 @@ trait CragDao extends Repository[Crag] {
 
   }
 
+  override def delete(crag: Revisioned[Crag]) = ActionT { session =>
+    if (false) { ConcurrentUpdate().left } else { crag.right }
+  }
+
+  override def update(cragRev: Revisioned[Crag]) = ActionT { session =>
+    implicit val connection = session.dbConnection
+    try {
+
+      val currentRevision: Option[Revisioned[Crag]] = get(cragRev.model.name).runWith(session)
+      currentRevision match {
+        case None => ConcurrentUpdate().left
+        case Some(latest) if latest.revision != cragRev.revision => ConcurrentUpdate().left
+        case _ =>
+          val crag = cragRev.model
+          val nextRevision: Long = SQL("SELECT nextval('crag_revision_seq');").as(scalar[Long].single)
+
+          SQL(
+            """
+            UPDATE crags SET
+              title = {title},
+              revision = {revision}
+            WHERE name = {name}
+            """
+          ).on(
+            "title"    -> crag.title,
+            "revision" -> nextRevision,
+            "name"     -> crag.name
+          ).execute()
+          new Revisioned[Crag](nextRevision, crag).right
+      }
+    } catch {
+      case e: SQLException => e.sqlError match {
+        case Some(SerializationFailure) => connection.rollback() ; ConcurrentUpdate().left
+        case _                          => connection.rollback() ; throw e
+      }
+      case e                            => connection.rollback() ; throw e
+    }
+
+  }
+
   def get(name: String): Action[Option[Revisioned[Crag]], TransactionReadCommitted] = Action { session =>
     implicit val connection = session.dbConnection
-    val result: Boolean = SQL("Select 1").execute()
-    None
+
+    try {
+      SQL(
+        """
+        SELECT name, title, revision FROM crags
+        WHERE name = {name}
+        """
+      ).on(
+        "name" -> name
+      ).as(revisionedCrag.singleOpt)
+    }
+  }
+
+  private val crag = {
+    str("crags.name") ~
+    str("crags.title") map {
+      case name~title => Crag.makeUnsafe(name, title)
+    }
+  }
+
+  private val revisionedCrag = {
+    crag ~ int("crags.revision") map {
+      case crag~revision => Revisioned[Crag](revision, crag)
+    }
   }
 
 }
