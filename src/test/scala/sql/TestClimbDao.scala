aad651a5c45d03767ded394d625631048d45e90f
Ian Murray <ian.c.murray@gmail.com>
2012-09-28 09:00:36 +0100

    Test get* tests on the climbdao

--- src/test/scala/sql/TestClimbDao.scala
+++ src/test/scala/sql/TestClimbDao.scala
@@ -35,7 +35,7 @@ class ClimbDaoTest extends FunSpec
   }
 
   after {
-    //cleanTables()
+    cleanTables()
   }
 
   private def newSession() = TestDatabaseSessions.newSession(TransactionRepeatableRead)
@@ -69,7 +69,85 @@ class ClimbDaoTest extends FunSpec
         latestClimb.revision should equal (updatedClimb.revision)
         latestClimb.model.name should equal ("harvest")
         latestClimb.model should equal (updatedClimb.model)
+      }
 
+      it("should return NotFound if the climb does not exist") {
+        val someClimb = run {
+          climbDao.get("burbage", "harvest")
+        }.swap getOrElse fail("Managed to find non-existeant Climb!")
+
+        someClimb should equal (NotFound())
+      }
+
+      it("should return NotFound if the climb's crag does not exist") {
+
+        // create harvest at burbage
+        run {
+          for {
+            _ <- CragDao.create(burbage)
+            _ <- ClimbDao.create(harvest)
+          } yield ()
+        } getOrElse fail("Failed to create fixture climb")
+
+        // and try to locate it at stanage
+        val someClimb = run {
+          climbDao.get("stanage", "harvest")
+        }.swap getOrElse fail("Managed to find non-existeant Climb!")
+
+        someClimb should equal (NotFound())
+      }
+    }
+
+    describe("The getOption action") {
+      it("should return the latest version of a climb that exists") {
+        // Create the crags
+        run {
+          CragDao.create(burbage)
+        } getOrElse fail("Failed to create fixture crag")
+
+        // Create a new Climb, and update it.
+        val updatedClimb = run {
+            for {
+              newClimb <- climbDao.create(harvest)
+              val update = Revisioned[Climb](newClimb.revision, newHarvest)
+              updatedClimb <- climbDao.update(update)
+            } yield updatedClimb
+        } getOrElse fail("Unable to create test fixture")
+
+        // Get the Climb, and check the results
+        val latestClimb = run {
+          climbDao.getOption("burbage", "harvest")
+        } getOrElse fail("No climb found.")
+
+        latestClimb should not equal (None)
+        latestClimb.get.revision should equal (updatedClimb.revision)
+        latestClimb.get.model should equal (updatedClimb.model)
+      }
+
+      it("should return None if the climb does not exist") {
+        val someClimb = run {
+          climbDao.getOption("burbage", "harvest")
+        } getOrElse fail("Failed to run getOption")
+
+        someClimb should equal (None)
+      }
+
+      it("should return None if the climb's crag does not exist") {
+
+        // create harvest at burbage
+        run {
+          for {
+            _ <- CragDao.create(burbage)
+            _ <- ClimbDao.create(harvest)
+          } yield ()
+        } getOrElse fail("Failed to create fixture climb")
+
+        // and try to locate it at stanage
+        val someClimb = run {
+          climbDao.getOption("stanage", "harvest")
+        } getOrElse fail("Failed to run getOption")
+
+        someClimb should equal (None)
       }
     }
 
