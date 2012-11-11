1cc57d35d415e1945cb98c94b5531ee0b57d7d1c
Ian Murray <ian.c.murray@gmail.com>
2012-11-11 12:17:53 +0000

    Split unmarshalling from marshalling
    Won't be unmarshall domain models directly soon.

--- src/main/scala/restApi/Marshallers.scala
+++ src/main/scala/restApi/Marshallers.scala
@@ -15,38 +15,47 @@ import freeclimb.api._
 import freeclimb.models._
 import freeclimb.restApi.ModelJson._
 
-class BasicModelMarshallers(val prettyPrint: Boolean) extends ModelMarshallers {
+class BasicModelMarshallers(val prettyPrint: Boolean) extends ModelMarshallers
+                                                      with RestResourceUnMarshallers
+                                                      with MimeTypes {
   override lazy val printer = if (prettyPrint) { PrettyPrinter } else { CompactPrinter }
 }
 
-trait ModelMarshallers {
-
-  type RichValidation[E,A] = \/[Map[String,NonEmptyList[E]], A]
-
-  def printer: JsonPrinter
+trait MimeTypes {
 
   val `application/hal+json` = MediaTypes.register(new ApplicationMediaType("hal+json", "json"))
+
+}
+
+trait ModelMarshallers { this: MimeTypes =>
+
+  def printer: JsonPrinter
 
   implicit val cragMarshaller = modelMarshaller[Crag]
   implicit val revisionedCragMarshaller = modelMarshaller[Revisioned[Crag]]
   implicit val climbMarshaller = modelMarshaller[Climb]
   implicit val revisionedClimbMarshaller = modelMarshaller[Revisioned[Climb]]
 
-  implicit val cragUnmarshaller = modelUnmarshaller[RichValidation[String, Crag]]
-  implicit val revisionedCragUnMarshaller = modelUnmarshaller[RichValidation[String, Revisioned[Crag]]]
-
   private def modelMarshaller[T : RootJsonWriter]: Marshaller[T] =
     Marshaller.delegate[T, String](`application/json`, `application/hal+json`) { value =>
       printer(value.toJson)
     }
+  
+  implicit val errorMapMarshaller: Marshaller[Map[String,NonEmptyList[String]]] =
+    Marshaller.delegate[Map[String,NonEmptyList[String]], String](`application/json`, `application/hal+json`) { value =>
+      printer(value.mapValues(_.toList).toJson)
+    }
+}
+trait RestResourceUnMarshallers { this: MimeTypes =>
+
+  type RichValidation[E,A] = \/[Map[String,NonEmptyList[E]], A]
+
+  implicit val cragUnmarshaller = modelUnmarshaller[RichValidation[String, Crag]]
+  implicit val revisionedCragUnMarshaller = modelUnmarshaller[RichValidation[String, Revisioned[Crag]]]
 
   private def modelUnmarshaller[T : RootJsonReader]: Unmarshaller[T] =
     Unmarshaller.delegate[String, T](`application/json`, `application/hal+json`) { string =>
       string.asJson.convertTo[T]
     }
 
-  implicit val errorMapMarshaller: Marshaller[Map[String,NonEmptyList[String]]] =
-    Marshaller.delegate[Map[String,NonEmptyList[String]], String](`application/json`, `application/hal+json`) { value =>
-      printer(value.mapValues(_.toList).toJson)
-    }
 }
