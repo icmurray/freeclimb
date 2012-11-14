f01fa3dbfccd393bdc99108348aee489e5d237b8
Ian Murray <ian.c.murray@gmail.com>
2012-11-14 11:37:59 +0000

    Further tests for GETing a Crag

--- src/test/scala/restApi/TestService.scala
+++ src/test/scala/restApi/TestService.scala
@@ -3,8 +3,12 @@ package freeclimb.restApi
 import org.scalatest.{FunSpec, BeforeAndAfter}
 import org.scalatest.matchers.ShouldMatchers
 
-import spray.testkit.ScalatestRouteTest
+import spray.httpx.unmarshalling._
+import spray.http.MediaTypes._
+import spray.json._
+import spray.json.DefaultJsonProtocol._
 import spray.routing.HttpService
+import spray.testkit.ScalatestRouteTest
 import spray.http.StatusCodes._
 
 import freeclimb.models._
@@ -15,6 +19,7 @@ class ServiceTest extends FunSpec
                      with Routes
                      with BeforeAndAfter
                      with ShouldMatchers
+                     with MimeTypes
                      with ScalatestRouteTest {
 
   override val api = new CrudApiMock()
@@ -51,8 +56,28 @@ class ServiceTest extends FunSpec
           }
         }
 
-        it("Should contain the revision in the representation") (pending)
-        it("Should describe the requested crag as JSON") (pending)
+        it("Should contain the revision in the representation") {
+          Get("/crags/burbage") ~> routes ~> check {
+            val json = entityAs[JsObject]
+            val revision = json.fields.get("revision")
+            revision should not equal (None)
+            revision.get.asInstanceOf[JsNumber].value should equal (1L)
+          }
+        }
+
+        it("Should describe the requested crag as JSON") {
+          Get("/crags/burbage") ~> routes ~> check {
+            val json = entityAs[JsObject]
+
+            val name = json.fields.get("name")
+            name should not equal (None)
+            name.get.asInstanceOf[JsString].value should equal(burbage.name)
+
+            val title = json.fields.get("title")
+            title should not equal (None)
+            title.get.asInstanceOf[JsString].value should equal(burbage.title)
+          }
+        }
 
       }
     }
@@ -60,4 +85,8 @@ class ServiceTest extends FunSpec
 
   private def burbage = Crag.makeUnsafe("burbage", "Burbage")
 
+  implicit private val JsonUnmarshaller: Unmarshaller[JsObject] =
+    Unmarshaller.delegate[String, JsObject](`application/json`, `application/hal+json`) { string =>
+      string.asJson.asJsObject
+    }
 }
