7b159cedc63cc3f41ff56b0569da204ffd49da80
Ian Murray <ian.c.murray@gmail.com>
2012-12-11 20:11:29 +0000

    Test the FakeClimbDao with the new ClimbDaoSpec
    Obviously not much is passing because the fake isn't fully implemented.

--- src/test/scala/common/FakeClimbDao.scala
+++ src/test/scala/common/FakeClimbDao.scala
@@ -1,8 +1,13 @@
 package freeclimb.test.common
 
+import scalaz._
+import Scalaz._
+
 import freeclimb.api._
 import freeclimb.models._
 import freeclimb.sql._
+
+import freeclimb.test.sql.ClimbDaoSpec
 
 class FakeClimbDao(db: FakeDb) extends ClimbDao {
 
@@ -23,3 +28,25 @@ class FakeClimbDao(db: FakeDb) extends ClimbDao {
   private def TODO: Nothing = throw new UnsupportedOperationException("Not implemented")
 }
 
+class FakeClimbDaoTest extends ClimbDaoSpec {
+
+  override val runner = new ActionRunner {
+    def run[M[+_],A,I <: IsolationLevel, W <: List[ActionEvent]](action: ActionT[M,A,I,W])
+                                                                (implicit F: Failable[M[_]], M: Functor[M], m: Manifest[I]): M[A] = {
+      val session = new DbSession[I] { val dbConnection = null }
+      val result = action(session)
+
+      // Discard the logged events from the final result
+      result map { _._2 }
+    }
+  }
+
+  override def withDaos(testCode: (CragDao, ClimbDao) => Any) {
+    val db = new FakeDb()
+    val cragDao = new CragDaoMock(db)
+    val climbDao = new FakeClimbDao(db)
+    testCode(cragDao, climbDao)
+  }
+
+
+}
