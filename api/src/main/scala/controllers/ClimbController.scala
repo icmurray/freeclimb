5f3c0dddf3c54a706dbdc3d445c3e24ff1b25aaf
Ian Murray <ian.c.murray@gmail.com>
2013-08-02 18:21:17 +0100

    No idea what these changes are
    It's been so long since I looked at this code!

--- api/src/main/scala/controllers/ClimbController.scala
+++ api/src/main/scala/controllers/ClimbController.scala
@@ -2,8 +2,14 @@ package org.freeclimbers.api.controllers
 
 import scala.concurrent.{Future, ExecutionContext}
 
+import spray.http.HttpResponse
+import spray.http.HttpHeader
+import spray.http.StatusCode
+import spray.http.StatusCodes._
+
 import org.freeclimbers.core.dal.ClimbRepository
 import org.freeclimbers.core.models.Climb
+import org.freeclimbers.core.services.{ServiceFailure,ServiceEvent}
 import org.freeclimbers.api.{PageLimits, Page, PageLinker}
 
 trait ClimbControllerComponent {
@@ -11,7 +17,15 @@ trait ClimbControllerComponent {
 }
 
 trait ClimbController {
-  def getPage(paging: PageLimits, urlFor: PageLinker): Future[Page[Climb]]
+
+  type Response[T] = Future[(StatusCode, List[HttpHeader], T)]
+
+  def getPage(paging: PageLimits, urlFor: PageLinker): Response[Page[Climb]]
+  def getClimb(id: Long): Future[HttpResponse]
+  def updateClimb(id: Long, newValue: Climb): Future[HttpResponse]
+  def moveClimb(id: Long, cragId: Long): Future[HttpResponse]
+  def latestChanges: Future[HttpResponse]
+  def changes(page: Long): Future[HttpResponse]
 }
 
 class DefaultClimbController(
@@ -20,10 +34,16 @@ class DefaultClimbController(
 
   private implicit val _ec = ec
 
-  override def getPage(paging: PageLimits, urlFor: PageLinker): Future[Page[Climb]] = {
+  override def getPage(paging: PageLimits, urlFor: PageLinker) = {
     for {
       (climbs, count) <- climbRepo.getPage(paging.limit, paging.offset)
-    } yield Page(count, climbs, paging, urlFor)
+    } yield (OK, Nil, Page(count, climbs, paging, urlFor))
   }
+
+  override def getClimb(id: Long) = ???
+  override def updateClimb(id: Long, newValue: Climb) = ???
+  override def moveClimb(id: Long, cragId: Long) = ???
+  override def latestChanges = ???
+  override def changes(page: Long) = ???
 
 }
