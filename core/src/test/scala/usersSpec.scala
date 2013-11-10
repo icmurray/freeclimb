981cf8e83077d7fe78164fdb83e1f73a153fe8bc
Ian Murray <ian.c.murray@gmail.com>
2013-11-10 18:46:13 +0000

    Tidy use of Await in tests.

--- core/src/test/scala/usersSpec.scala
+++ core/src/test/scala/usersSpec.scala
@@ -21,32 +21,33 @@ class UserServiceSpec extends FlatSpec with ShouldMatchers {
 
   "A UserService" should "register new users" in {
     withUsersModule { module =>
-      val userF = module.users.register(
-        Email("test@example.com"), "Test", "User", PlainText("pass"))
-      val user = Await.result(userF, 2.seconds)
+      val user = blockFor {
+        module.users.register(
+          Email("test@example.com"), "Test", "User", PlainText("pass"))
+      }
       user.isSuccess should equal (true)
     }
   }
 
   "A UserService" should "not re-register an existing user" in {
     withUsersModule { module =>
-      val userF = module.users.register(
-        Email("test@example.com"), "Test", "User", PlainText("pass"))
-      val user = Await.result(userF, 1.seconds)
+      val user = blockFor {
+        module.users.register( Email("test@example.com"), "Test", "User", PlainText("pass"))
+      }
       user.isSuccess should equal (true)
 
-      val sameUserF = module.users.register(
-        Email(" test@example.com "), "Test", "User", PlainText("pass"))
-      val sameUser = Await.result(sameUserF, 1.seconds)
+      val sameUser = blockFor {
+        module.users.register(Email(" test@example.com "), "Different", "Name", PlainText("pass"))
+      }
       sameUser.isSuccess should equal(false)
     }
   }
 
   "A UserService" should "not allow blank emails" in {
     withUsersModule { module =>
-      val userF = module.users.register(
-        Email("  "), "Test", "User", PlainText("pass"))
-      val user = Await.result(userF, 1.seconds)
+      val user = blockFor {
+        module.users.register(Email("  "), "Test", "User", PlainText("pass"))
+      }
       user.isSuccess should equal (false)
     }
   }
@@ -55,16 +56,17 @@ class UserServiceSpec extends FlatSpec with ShouldMatchers {
     withUsersModule { module =>
       implicit val ec = module.ec
 
-      val authF = for {
-        userV <- module.users.register(
-          Email("test@example.com"), "Test", "User", PlainText("pass")
-        )
-        user = userV.getOrElse(throw new RuntimeException())
+      val auth = blockFor {
+        for {
+          userV <- module.users.register(
+            Email("test@example.com"), "Test", "User", PlainText("pass")
+          )
+          user = userV.getOrElse(throw new RuntimeException())
 
-        auth <- module.users.authenticate(user.email, PlainText("pass"))
-      } yield auth
+          auth <- module.users.authenticate(user.email, PlainText("pass"))
+        } yield auth
+      }
 
-      val auth = Await.result(authF, 2.seconds)
       auth should not equal (None)
       auth.get.email should equal (Email("test@example.com"))
     }
@@ -74,16 +76,17 @@ class UserServiceSpec extends FlatSpec with ShouldMatchers {
     withUsersModule { module =>
       implicit val ec = module.ec
 
-      val authF = for {
-        userV <- module.users.register(
-          Email("test@example.com"), "Test", "User", PlainText("pass")
-        )
-        user = userV.getOrElse(throw new RuntimeException())
+      val auth = blockFor {
+        for {
+          userV <- module.users.register(
+            Email("test@example.com"), "Test", "User", PlainText("pass")
+          )
+          user = userV.getOrElse(throw new RuntimeException())
 
-        auth <- module.users.authenticate(user.email, PlainText("WRONG"))
-      } yield auth
+          auth <- module.users.authenticate(user.email, PlainText("WRONG"))
+        } yield auth
+      }
 
-      val auth = Await.result(authF, 2.seconds)
       auth should equal (None)
     }
   }
@@ -92,29 +95,31 @@ class UserServiceSpec extends FlatSpec with ShouldMatchers {
     withUsersModule { module =>
       implicit val ec = module.ec
 
-      val tokenF = for {
-        userV <- module.users.register(
-          Email("test@example.com"), "Test", "User", PlainText("pass")
-        )
-        user = userV.getOrElse(throw new RuntimeException())
+      val token = blockFor {
+        for {
+          userV <- module.users.register(
+            Email("test@example.com"), "Test", "User", PlainText("pass")
+          )
+          user = userV.getOrElse(throw new RuntimeException())
 
-        tokenO <- module.users.login(user.email, PlainText("pass"))
-      } yield tokenO.get
+          tokenO <- module.users.login(user.email, PlainText("pass"))
+        } yield tokenO.get
+      }
 
-      val token = Await.result(tokenF, 2.seconds)
-
-      val authF = module.users.authenticate(Email("test@example.com"), token)
-      val auth = Await.result(authF, 2.seconds)
+      val auth = blockFor {
+        module.users.authenticate(Email("test@example.com"), token)
+      }
 
       auth should not equal (None)
       auth.get.email should equal (Email("test@example.com"))
 
-      Await.result(
-        module.users.logout(Email("test@example.com"), token),
-        2.seconds)
+      blockFor {
+        module.users.logout(Email("test@example.com"), token)
+      }
 
-      val authAgainF = module.users.authenticate(Email("test@example.com"), token)
-      val authAgain = Await.result(authAgainF, 2.seconds)
+      val authAgain = blockFor {
+        module.users.authenticate(Email("test@example.com"), token)
+      }
       authAgain should equal (None)
     }
   }
@@ -123,22 +128,27 @@ class UserServiceSpec extends FlatSpec with ShouldMatchers {
     withUsersModule { module =>
       implicit val ec = module.ec
 
-      val tokenF = for {
-        userV <- module.users.register(
-          Email("test@example.com"), "Test", "User", PlainText("pass")
-        )
-        user = userV.getOrElse(throw new RuntimeException())
+      val token = blockFor {
+        for {
+          userV <- module.users.register(
+            Email("test@example.com"), "Test", "User", PlainText("pass")
+          )
+          user = userV.getOrElse(throw new RuntimeException())
 
-        tokenO <- module.users.login(user.email, PlainText("pass"))
-      } yield tokenO.get
+          tokenO <- module.users.login(user.email, PlainText("pass"))
+        } yield tokenO.get
+      }
 
-      val token = Await.result(tokenF, 2.seconds)
-
-      val authF = module.users.authenticate(Email("not-test@example.com"), token)
-      val auth = Await.result(authF, 2.seconds)
+      val auth = blockFor {
+        module.users.authenticate(Email("not-test@example.com"), token)
+      }
 
       auth should equal (None)
     }
+  }
+
+  private def blockFor[T](f: => Future[T]): T = {
+    Await.result(f, 2.seconds)
   }
 
   private def withUsersModule(f: UsersModule[Future] with ActorSystemModule => Unit) = {
