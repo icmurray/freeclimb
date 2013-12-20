ba13275b4b00e9a417a08343b1d7f0d79c0f6382
Ian Murray <ian.c.murray@gmail.com>
2013-12-20 19:41:18 +0000

    Add the crag routes to production.

--- api/src/main/scala/routes.scala
+++ api/src/main/scala/routes.scala
@@ -3,17 +3,19 @@ package org.freeclimbers.api
 import scala.concurrent.Future
 import scala.language.higherKinds
 
-import org.freeclimbers.core.{UsersModule, ClimbsModule}
+import org.freeclimbers.core.{UsersModule, ClimbsModule, CragsModule}
 
 /**
  * Convenience trait that brings togeher all the route traits.
  */
 trait AllRoutes[M[+_]] extends UserRoutes[M]
-                          with ClimbRoutes[M] {
+                          with ClimbRoutes[M]
+                          with CragRoutes[M] {
   this: UsersModule[M] with HigherKindedUtils[M]
+                       with CragsModule[M]
                        with ClimbsModule[M] =>
 
-  lazy val routes = userRoutes ~ climbRoutes
+  lazy val routes = userRoutes ~ climbRoutes ~ cragRoutes
 
 }
 
@@ -22,5 +24,6 @@ trait AllRoutes[M[+_]] extends UserRoutes[M]
  */
 trait ProductionRoutes extends AllRoutes[Future]
                           with FutureUtils {
-  this: UsersModule[Future] with ClimbsModule[Future] =>
+  this: UsersModule[Future] with ClimbsModule[Future]
+                            with CragsModule[Future] =>
 }
